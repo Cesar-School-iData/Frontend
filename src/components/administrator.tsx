7559eaecba58c4512fcf6c172c6158c0a5e28b12
jpedro7 <git@github.com>
2022-11-11 18:09:33 -0300

    fix identation

--- src/components/administrator.tsx
+++ src/components/administrator.tsx
@@ -5,63 +5,63 @@ import Modal from '@mui/material/Modal';
 import TextField from '@mui/material/TextField';
 
 const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
 };
 
 export default function BasicModal() {
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-    return (
-        <div>
-            <div className='flex justify-center sm:px-6 lg:px-8'>
-                <button className="text-gray-700 bg-white border-gray-300 border shadow-sm hover:border hover:border-gray-600 px-14 py-1 rounded-full" onClick={handleOpen}>Sou um administrador</button>
+  return (
+    <div>
+      <div className='flex justify-center sm:px-6 lg:px-8'>
+        <button className="text-gray-700 bg-white border-gray-300 border shadow-sm hover:border hover:border-gray-600 px-14 py-1 rounded-full" onClick={handleOpen}>Sou um administrador</button>
+      </div>
+      <Modal
+        open={open}
+      >
+        <Box sx={style}>
+          <Box
+            component="form"
+            sx={{
+              '& .MuiTextField-root': { m: 1, width: '35ch' },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <div>
+              <TextField
+                required
+                id="outlined-required"
+                label="Email"
+              />
+              <TextField
+                id="outlined-password-input"
+                label="Senha"
+                type="password"
+                autoComplete="current-password"
+              />
             </div>
-            <Modal
-                open={open}
-            >
-                <Box sx={style}>
-                    <Box
-                        component="form"
-                        sx={{
-                            '& .MuiTextField-root': { m: 1, width: '35ch' },
-                        }}
-                        noValidate
-                        autoComplete="off"
-                    >
-                        <div>
-                            <TextField
-                                required
-                                id="outlined-required"
-                                label="Email"
-                            />
-                            <TextField
-                                id="outlined-password-input"
-                                label="Senha"
-                                type="password"
-                                autoComplete="current-password"
-                            />
-                        </div>
-                        <header className="flex flex-col">
-                            <div className='mx-auto'>
-                                <Button className='rounded text-white font-semibold bg-black hover:bg-slate-600 py-2 px-5' onClick={handleClose}>entrar</Button>
-                            </div>
-                            <div className='mx-auto'>
-                                <Button className='rounded text-white font-semibold bg-black hover:bg-slate-600 py-2 px-5' onClick={handleClose}>cancelar</Button>
-                            </div>
-                        </header>
-                    </Box>
-                </Box>
-            </Modal>
-        </div>
-    );
+            <header className="flex flex-col">
+              <div className='mx-auto'>
+                <Button className='rounded text-white font-semibold bg-black hover:bg-slate-600 py-2 px-5' onClick={handleClose}>entrar</Button>
+              </div>
+              <div className='mx-auto'>
+                <Button className='rounded text-white font-semibold bg-black hover:bg-slate-600 py-2 px-5' onClick={handleClose}>cancelar</Button>
+              </div>
+            </header>
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
 }
--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -6,28 +6,28 @@ function filter() {
       <div className="px-8 max-w-md h-96 bg-gray-300 rounded-3xl border border-gray-400 shadow-sm space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
         <div className="text-center space-y-2 sm:text-left">
           <div className="space-y-10">
-              <p className="text-lg text-gray-500 font-bold">
+            <p className="text-lg text-gray-500 font-bold">
               Andar
-              </p>
-              <p className="text-lg text-gray-500 font-bold">
+            </p>
+            <p className="text-lg text-gray-500 font-bold">
               Espaço
-              </p>
-              <p className="text-lg text-gray-500 font-bold">
+            </p>
+            <p className="text-lg text-gray-500 font-bold">
               Numeração
-              </p>
-              <p className="text-lg text-gray-500 font-bold">
+            </p>
+            <p className="text-lg text-gray-500 font-bold">
               Categoria do problema
-              </p>
-            </div>
+            </p>
+          </div>
         </div>
         <div className='py-4 px-8 w-52 h-64 bg-gray-400 rounded-3xl shadow-3xl'>
-            <div className="text-center space-y-2 sm:text-left">
-                <div className="space-y-0.5">
-                    <p className="text-white font-medium">
-                    Selecione
-                    </p>
-                </div>
+          <div className="text-center space-y-2 sm:text-left">
+            <div className="space-y-0.5">
+              <p className="text-white font-medium">
+                Selecione
+              </p>
             </div>
+          </div>
         </div>
       </div>
     </div>
