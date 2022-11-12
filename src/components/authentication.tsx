d5bc33fbeeab02ff336e4c2c593766e7dba16b4e
Jacquelin Busch <jacq3002@hotmail.com>
2022-11-12 01:16:36 -0300

    atualizações

--- src/components/authentication.tsx
+++ src/components/authentication.tsx
@@ -1,71 +1,84 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
-import TextField from '@mui/material/TextField';
+import { Dialog, Transition } from '@headlessui/react'
+import React from "react";
+import { Fragment, useState } from 'react'
 
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
+function authentification() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [isOpenSection, setIsOpenSection] = useState(true)
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function closeModalNextSection() {
+        setIsOpenSection(false)
+    }
+
+    function openModalNextSection() {
+        setIsOpenSection(true)
+    }
+
+    function loading() {
+        setIsOpenSection(true)
+    }
+
+    return (
+        <>
+            <div className='text-gray-400'>
+                <button onClick={openModal} className="flex my-auto">
+                    <img className='voteButtons hover:text-blue-400' src="https://img.icons8.com/sf-regular/48/null/up.png" />
+                    <span className='text-xs font-bold text-black my-auto'>0</span>
+                </button>
+            </div>
+            <Transition appear show={isOpen} as={Fragment}>
+                <Dialog as="div" onClose={closeModal}>
+                    <Transition.Child
+                        as={Fragment}
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100 scale-100"
+                        leaveFrom="opacity-100 scale-100"
+                        leaveTo="opacity-0"
+                        enter="ease-out duration-300"
+                        leave="ease-in duration-200">
+                        <div className="fixed inset-0 bg-black bg-opacity-25">
+                            <div className='bg-white rounded-3xl shadow-2xl container max-w-sm h-80 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+                                <header className="flex flex-col px-5 py-11">
+                                    <button className='absolute top-2 right-4 rounded-full text-black font-semibold hover:bg-blue-50 py-0 px-2' onClick={closeModal}>X</button>
+                                    <p className='text-slate-600 font-medium'>Para prosseguir, por favor, informe o seu e-mail institucional</p>
+                                    <input type="text" name="email" className="w-full rounded-lg border mt-3 py-3 px-4 border-gray-600"></input>
+                                    <button className='absolute bottom-11 inset-x-24 rounded-full text-white font-semibold bg-black hover:bg-slate-600 py-2 px-5' onClick={openModalNextSection}>Avançar</button>
+                                    <div className='bg-gray-600 rounded-full h-3 w-3 absolute bottom-3 inset-x-44'></div>
+                                    <div className='bg-gray-300 rounded-full h-3 w-3 absolute bottom-3 inset-x-52'></div>
+                                </header>
+                            </div>
+                        </div>
+                    </Transition.Child>
+                </Dialog>
+            </Transition >
+        </>
+    );
 };
 
-export default function BasicModal() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+export default authentification
 
-  return (
-    <div>
-      <div className='text-gray-400'>
-        <button onClick={handleOpen} className="flex my-auto">
-          <img className='voteButtons hover:text-blue-400' src="https://img.icons8.com/sf-regular/48/null/up.png" />
-          <span className='text-xs font-bold text-black my-auto'>0</span>
-        </button>
-      </div>
-      <Modal
-        open={open}
-      >
-        <Box sx={style}>
-          <Box
-            component="form"
-            sx={{
-              '& .MuiTextField-root': { m: 1, width: '35ch' },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <div>
-              <TextField
-                required
-                id="outlined-required"
-                label="Email"
-              />
-              <TextField
-                id="outlined-read-only-input"
-                label="Observação"
-                InputProps={{
-                  readOnly: false,
-                }}
-              />
-            </div>
-            <header className="flex flex-col">
-              <div className='mx-auto'>
-                <Button className='rounded text-white font-semibold bg-black hover:bg-slate-600 py-2 px-5' onClick={handleClose}>enviar</Button>
-              </div>
-              <div className='mx-auto'>
-                <Button className='rounded text-white font-semibold bg-black hover:bg-slate-600 py-2 px-5' onClick={handleClose}>cancelar</Button>
-              </div>
-            </header>
-          </Box>
-        </Box>
-      </Modal>
-    </div>
-  );
-}
+/*
+<Transition appear show={isOpenSection} as={Fragment}>
+                <Dialog as="div" onClose={closeModalNextSection}>
+                    <div className='bg-white rounded-3xl shadow-2xl container max-w-sm h-80 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+                        <header className="flex flex-col px-5 py-11">
+                            <button className='absolute top-2 left-4 rounded-full text-black font-semibold hover:bg-blue-50 py-0 px-2' onClick={closeModalNextSection}>-</button>
+                            <button className='absolute top-2 right-4 rounded-full text-black font-semibold hover:bg-blue-50 py-0 px-2' onClick={closeModal}>X</button>
+                            <p className='text-slate-600 font-medium'>Deseja incluir um comentário adicional sobre o problema?</p>
+                            <input type="text" name="observacao" className="w-full rounded-lg border mt-3 py-3 px-4 border-gray-600"></input>
+                            <button className='absolute bottom-11 inset-x-24 rounded-full text-white font-semibold bg-black hover:bg-slate-600 py-2 px-5' onClick={loading}>Concluir</button>
+                            <div className='bg-gray-300 rounded-full h-3 w-3 absolute bottom-3 inset-x-44'></div>
+                            <div className='bg-gray-600 rounded-full h-3 w-3 absolute bottom-3 inset-x-52'></div>
+                        </header>
+                    </div>
+                </Dialog>
+            </Transition>
+*/
--- src/components/problemCard.tsx
+++ src/components/problemCard.tsx
@@ -13,7 +13,7 @@ function problemCard({/* { problem } */ }) {
                     </div>
                     <hr />
                     <div className='flex justify-between'>
-                        <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Ver detalhes</button>
+                        <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Mais detalhes</button>
                         <Authentication />
                     </div>
                 </div>
